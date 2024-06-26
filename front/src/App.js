c10c8daba9696a88bee2582756dbc0ba95a88d57
JANGHYUNSU <satgat981111@gmail.com>
2024-06-26 18:21:52 +0900

    commit jang

--- front/src/App.js
+++ front/src/App.js
@@ -1,13 +1,17 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Root from "./pages/Root";
+import About from "./pages/About";
 
 export default function App() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <Root />,
-      children: [{ path: "/", element: <Home /> }],
+      children: [
+        { path: "/", element: <Home /> },
+        { path: "/about", element: <About /> },
+      ],
     },
   ]);
   return <RouterProvider router={router} />;
