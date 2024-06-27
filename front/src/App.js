7fda583a0e49d0232c13de9d69db50c187dac7e2
seo <test@test.com>
2024-06-27 17:48:25 +0900

    commit kim 0627 2

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,8 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Home from "./pages/Home";
-import Root from "./pages/Root";
-import About from "./pages/About";
+import Home from "./pages/Home.jsx";
+import Root from "./pages/Root.jsx";
+import About from "./pages/About.jsx";
+import Shop from "./pages/Shop.jsx";
 
 export default function App() {
   const router = createBrowserRouter([
