22e3175f61110e1bb138624ca63873d122c5f4cd
JANGHYUNSU <satgat981111@gmail.com>
2024-06-28 18:10:31 +0900

    commit jang

--- front/src/components/SubTitle.jsx
+++ front/src/components/SubTitle.jsx
@@ -4,7 +4,7 @@ import "../css/style.css";
 export default function SubTitle({ title }) {
   return (
     <div className="sub-title">
-      <h2 style={{ fontWeight: "700" }}>{title}</h2>
+      <h2>{title}</h2>
     </div>
   );
 }
