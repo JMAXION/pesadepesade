822e609b8cb67d2525e8914e9aa8c54ebda1c65a
test <example@example.com>
2024-06-28 18:25:46 +0900

    commit

--- front/src/pages/Login.jsx
+++ front/src/pages/Login.jsx
@@ -1,50 +1,79 @@
+import "../css/hyerim.css";
+
 export default function Login() {
   return (
-    <div className="content">
+    <div className="hyerim_content">
       <div className="login">
-        <h2>Login</h2>
-        <div>로그인</div>
+        <h2 className="login_title">Login</h2>
+        <div className="login_title_kor">로그인</div>
         <form action="">
           <ul>
             <li>
-              <input type="text" name="userId" placeholder="ID" />
+              <input
+                className="login_input"
+                type="text"
+                name="userId"
+                placeholder="  ID"
+              />
             </li>
             <li>
-              <input type="password" name="userPass" placeholder="Password" />
+              <input
+                className="login_input"
+                type="password"
+                name="userPass"
+                placeholder="  Password"
+              />
             </li>
             <li>
-              <button type="submit">Login</button>
+              <button className="login_button" type="submit">
+                Login
+              </button>
             </li>
           </ul>
-          <div>
+          <div className="login_find">
             <a href="#">아이디찾기</a>
             <a href="#">비밀번호찾기</a>
           </div>
           <div>
-            <ul>
-              <li>
-                <a href="#">
-                  <img src="/memberimages/kakao.png" /> Kakao
+            <ul className="login_sns">
+              <li className="login_sns_button">
+                <a className="login_sns_link" href="#">
+                  <img
+                    className="login_sns_image"
+                    src="/memberimages/kakao.png"
+                  />
+                  &nbsp;Kakao
                 </a>
               </li>
-              <li>
-                <a href="#">
-                  <img src="/memberimages/naver.png" /> Naver
+              <li className="login_sns_button">
+                <a className="login_sns_link" href="#">
+                  <img
+                    className="login_sns_image"
+                    src="/memberimages/naver.png"
+                  />
+                  &nbsp;Naver
                 </a>
               </li>
             </ul>
           </div>
+          <div className="login_bar"></div>
           <div>
-            <ul>
-              <li>
+            <ul className="login_join_box">
+              <li className="login_join_button">
                 <a href="">회원가입</a>
               </li>
-              <li>
+              <li className="login_join_button">
                 <a href="">비회원 주문조회</a>
               </li>
             </ul>
-            <p>* 신규회원 3천원 쿠폰&카카오 플친 추가 천원 쿠폰증정</p>
-            <p>* 공식 온라인 스토어 단독 혜택 모든 제품 구매시 무료배송</p>
+            <div className="login_description_box">
+              <p className="login_description">
+                * 신규회원 3천원 쿠폰&카카오 플친 추가 천원 쿠폰증정
+              </p>
+              <p className="login_description">
+                * 공식 온라인 스토어 단독 혜택 모든 제품 구매시 무료배송
+              </p>
+            </div>
           </div>
         </form>
       </div>
