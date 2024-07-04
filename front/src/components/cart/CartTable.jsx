0ec70c748133b664d549a02261402157fc90e9a3
gusn9719 <gusn9719@gmail.com>
2024-07-04 10:09:53 +0900

    0704Lim

--- front/src/components/cart/CartTable.jsx
+++ front/src/components/cart/CartTable.jsx
@@ -11,7 +11,13 @@ export default function CartTable () {
                          </tr>
                     </thead>
                     <tbody>
-
+                         <tr>
+                              <td></td>
+                              <td></td>
+                              <td></td>
+                              <td></td>
+                              <td></td>
+                         </tr>
                     </tbody>
                </table>
           </div>
