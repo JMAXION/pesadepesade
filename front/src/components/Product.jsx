c333ace2a02520bf21c7b9a1c6015813e537b3a9
MS <test@test.com>
2024-07-05 18:45:38 +0900

    hyunsung update

--- front/src/components/Product.jsx
+++ front/src/components/Product.jsx
@@ -1,11 +1,13 @@
-import React, { useEffect, useState } from "react";
+// Product.jsx
+import React, { useEffect, useState, useRef } from "react";
 import axios from 'axios';
 import '../css/product.css';
 import AddToCartModal from "./AddToCartModal";
 
 export default function Product({ name }) {
-  const [item, setItem] = useState(null); // 초기값을 null로 설정
-  const [selectedProduct, setSelectedProduct] = useState(null); // 선택된 상품 상태 추가
+  const [item, setItem] = useState(null);
+  const [selectedProduct, setSelectedProduct] = useState(null);
+  const productThumbnailRef = useRef(null);
 
   const url = 'http://127.0.0.1:8080/product';
 
@@ -14,47 +16,54 @@ export default function Product({ name }) {
       method: 'POST',
       url: url,
       data: { type: name }
-    }).then(result => setItem(result.data)); // 데이터를 받아와서 item 상태 업데이트
-  }, [name]); // name이 바뀔 때마다 useEffect가 실행됨
+    }).then(result => setItem(result.data));
+  }, [name]);
 
-  if (!item) {
-    return null; // item이 null이면 아무것도 렌더링하지 않음
-  }
+  useEffect(() => {
+    if (productThumbnailRef.current) {
+      $(productThumbnailRef.current).ripples({
+        resolution: 512,
+        dropRadius: 20,
+        perturbance: 0.04,
+      });
+    }
+  }, [item]);
 
-  // Add to cart 버튼 클릭 시 모달 열기
   const openModal = (product) => {
     setSelectedProduct(product);
   };
 
-  // 모달 닫기
   const closeModal = () => {
     setSelectedProduct(null);
   };
 
+  if (!item) {
+    return null;
+  }
+
   return (
     <div className="product-wrapper">
       <h1>{name}</h1>
-      <div className="product-thumbnail">
+      <div className="product-thumbnail" ref={productThumbnailRef}>
         <ul className="product-list">
           {item.map((obj, index) => (
-                <li className="product-list-li" key={index}>
-                  <div className="product-description">
-                  <strong className="product-name">
-                    <span>{obj.pname}</span>
-                  </strong>
-                  <span>{obj.pdetail}</span>
-                  <span><br />{obj.pprice.toLocaleString()} krw</span><br></br>
-                  <button  className="btn-text-cart" onClick={() => openModal(obj)}>Add to cart</button>
-                </div>
-            <div className="product-inner">
-                
+            <li className="product-list-li" key={index}>
+              <div className="product-description">
+                <strong className="product-name">
+                  <span>{obj.pname}</span>
+                </strong>
+                <span>{obj.pdetail}</span>
+                <span><br />{obj.pprice.toLocaleString()} krw</span><br></br>
+                <button className="btn-text-cart" onClick={() => openModal(obj)}>Add to cart</button>
+              </div>
+              <div className="product-inner">
                 <img src={obj.pimage} alt={obj.pname} />
               </div>
             </li>
           ))}
         </ul>
       </div>
-      
+
       {/* 모달 컴포넌트 */}
       {selectedProduct && (
         <AddToCartModal
