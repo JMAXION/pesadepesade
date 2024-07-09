c8e92fc0133a51d9766706c2f6327c860872319e
JANGHYUNSU <satgat981111@gmail.com>
2024-07-09 14:44:46 +0900

    commit jang

--- front/src/pages/JournalDetail.jsx
+++ front/src/pages/JournalDetail.jsx
@@ -31,102 +31,106 @@ export default function JournalDetail() {
 
   return (
     <div className="journal-detail">
-      <p className="journal-detail-title">
+      <div className="journal-detail-title">
         <img src={journal.jimg} alt="" className="journal-detail-title-img" />
-        <h2>{journal.jtitle}</h2>
-        <h3>{journal.jdate}</h3>
-      </p>
-      <ul>
-        <li>[BRAND STORY]</li>
-        <p>{journal.jbranddesc}</p>
-      </ul>
-      <div className="journal-slider">
-        <p>{journal.jcarouseltitle}</p>
-        <Swiper
-          spaceBetween={0}
-          slidesPerView={1}
-          pagination={{ clickable: true }}
-          loop={true}
-          autoplay={{ delay: 8000, disableOnInteraction: false }}
-          modules={[Pagination, Autoplay]}
-        >
-          {journal.cimg1 && (
-            <SwiperSlide>
-              <img
-                src={journal.cimg1}
-                alt="이미지 1"
-                className="slider-image"
-              />
-            </SwiperSlide>
-          )}
-          {journal.cimg2 && (
-            <SwiperSlide>
-              <img
-                src={journal.cimg2}
-                alt="이미지 2"
-                className="slider-image"
-              />
-            </SwiperSlide>
-          )}
-          {journal.cimg3 && (
-            <SwiperSlide>
-              <img
-                src={journal.cimg3}
-                alt="이미지 3"
-                className="slider-image"
-              />
-            </SwiperSlide>
-          )}
-          {journal.cimg4 && (
-            <SwiperSlide>
-              <img
-                src={journal.cimg4}
-                alt="이미지 4"
-                className="slider-image"
-              />
-            </SwiperSlide>
-          )}
-        </Swiper>
+        <p className="journal-detail-title-title">
+          <h2>{journal.jtitle}</h2>
+          <h3>{journal.jdate}</h3>
+        </p>
       </div>
-      <div className="journal-detail-desc">
-        <img src={journal.dimg1} alt="" className="journal-detail-desc-img" />
-        <ul className="journal-detail-desc-text">
-          <li>{journal.dtitle1}</li>
-          <li>{journal.ddesc1}</li>
+      <div className="journal-detail-text">
+        <div className="journal-detail-brandstory">
+          <li>[BRAND STORY]</li>
+          <p>{journal.jbranddesc}</p>
+        </div>
+        <div className="journal-slider">
+          <p>{journal.jcarouseltitle}</p>
+          <Swiper
+            spaceBetween={0}
+            slidesPerView={1}
+            pagination={{ clickable: true }}
+            loop={true}
+            autoplay={{ delay: 8000, disableOnInteraction: false }}
+            modules={[Pagination, Autoplay]}
+          >
+            {journal.cimg1 && (
+              <SwiperSlide>
+                <img
+                  src={journal.cimg1}
+                  alt="이미지 1"
+                  className="slider-image"
+                />
+              </SwiperSlide>
+            )}
+            {journal.cimg2 && (
+              <SwiperSlide>
+                <img
+                  src={journal.cimg2}
+                  alt="이미지 2"
+                  className="slider-image"
+                />
+              </SwiperSlide>
+            )}
+            {journal.cimg3 && (
+              <SwiperSlide>
+                <img
+                  src={journal.cimg3}
+                  alt="이미지 3"
+                  className="slider-image"
+                />
+              </SwiperSlide>
+            )}
+            {journal.cimg4 && (
+              <SwiperSlide>
+                <img
+                  src={journal.cimg4}
+                  alt="이미지 4"
+                  className="slider-image"
+                />
+              </SwiperSlide>
+            )}
+          </Swiper>
+        </div>
+        <div className="journal-detail-desc">
+          <img src={journal.dimg1} alt="" className="journal-detail-desc-img" />
+          <ul className="journal-detail-desc-text">
+            <li>{journal.dtitle1}</li>
+            <li>{journal.ddesc1}</li>
+          </ul>
+        </div>
+        <div className="journal-detail-desc">
+          <ul className="journal-detail-desc-text">
+            <li>{journal.dtitle2}</li>
+            <li>{journal.ddesc2}</li>
+          </ul>
+          <img src={journal.dimg2} alt="" className="journal-detail-desc-img" />
+        </div>
+        <div className="journal-detail-desc">
+          <img src={journal.dimg3} alt="" className="journal-detail-desc-img" />
+          <ul className="journal-detail-desc-text">
+            <li>{journal.dtitle3}</li>
+            <li>{journal.ddesc3}</li>
+          </ul>
+        </div>
+        <div className="journal-detail-desc">
+          <ul className="journal-detail-desc-text">
+            <li>{journal.dtitle4}</li>
+            <li>{journal.ddesc4}</li>
+          </ul>
+          <img src={journal.dimg4} alt="" className="journal-detail-desc-img" />
+        </div>
+        <div className="journal-detail-desc">
+          <img src={journal.dimg5} alt="" className="journal-detail-desc-img" />
+          <ul className="journal-detail-desc-text">
+            <li>{journal.dtitle5}</li>
+            <li>{journal.ddesc5}</li>
+          </ul>
+        </div>
+        <ul className="journal-detail-editor">
+          <li>[EDITOR SAY]</li>
+          <li>{journal.deditor}</li>
         </ul>
       </div>
-      <div className="journal-detail-desc">
-        <ul className="journal-detail-desc-text">
-          <li>{journal.dtitle2}</li>
-          <li>{journal.ddesc2}</li>
-        </ul>
-        <img src={journal.dimg2} alt="" className="journal-detail-desc-img" />
-      </div>
-      <div className="journal-detail-desc">
-        <img src={journal.dimg3} alt="" className="journal-detail-desc-img" />
-        <ul className="journal-detail-desc-text">
-          <li>{journal.dtitle3}</li>
-          <li>{journal.ddesc3}</li>
-        </ul>
-      </div>
-      <div className="journal-detail-desc">
-        <ul className="journal-detail-desc-text">
-          <li>{journal.dtitle4}</li>
-          <li>{journal.ddesc4}</li>
-        </ul>
-        <img src={journal.dimg4} alt="" className="journal-detail-desc-img" />
-      </div>
-      <div className="journal-detail-desc">
-        <img src={journal.dimg5} alt="" className="journal-detail-desc-img" />
-        <ul className="journal-detail-desc-text">
-          <li>{journal.dtitle5}</li>
-          <li>{journal.ddesc5}</li>
-        </ul>
-      </div>
-      <ul className="journal-detail-editor">
-        <li>[EDITOR SAY]</li>
-        <li>{journal.deditor}</li>
-      </ul>
     </div>
   );
 }
--- server/repository/journalRepository.js
+++ server/repository/journalRepository.js
@@ -13,7 +13,7 @@ j.jid as id, j.jcategory, j.jtitle, j.jdesc, j.jimg, j.jdate, j.jbranddesc, j.jc
 i.cimg1, i.cimg2, i.cimg3, i.cimg4, i.dimg1, i.dimg2, i.dimg3, i.dimg4, i.dimg5,
 d.dtitle1, d.dtitle2, d.dtitle3, d.dtitle4, d.dtitle5, d.ddesc1, d.ddesc2, d.ddesc3, d.ddesc4, d.ddesc5, d.deditor
 from journal_table j, detailimage_table i, journaldetail_table d 
-where j.jid = i.jid and i.jid = d.jid;
+where j.jid = i.jid and i.jid = d.jid and j.jid = ?;
 ;
   `;
   return db.execute(sql, [id]).then((result) => result[0][0]);
