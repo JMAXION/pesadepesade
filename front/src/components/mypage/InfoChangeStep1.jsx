29635a34e8fcb2758f16af9357768d4ae6619fe2
JANGHYUNSU <satgat981111@gmail.com>
2024-07-11 14:55:52 +0900

    commit jang

--- front/src/components/mypage/InfoChangeStep1.jsx
+++ front/src/components/mypage/InfoChangeStep1.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import SubTitle from "../../components/SubTitle";
+import { faFortAwesome } from "@fortawesome/free-brands-svg-icons";
+import { faLock } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function InfoChangeStep1({ nextstep }) {
   return (
@@ -7,9 +10,14 @@ export default function InfoChangeStep1({ nextstep }) {
       <SubTitle title="회원 정보 수정" />
       <div className="mypage-infochange">
         <p className="mypage-infochange-title">비밀번호 확인</p>
-        <p className="mypage-infochange-text">
-          홍길동 님의 회원정보를 안전하게 보호하기 위해 비밀번호를 한번 더
-          확인해 주세요.
+        <p className="mypage-infochange-main">
+          <FontAwesomeIcon icon={faLock} className="mypage-infochange-icon" />
+          <p>
+            <p className="mypage-infochange-text">
+              홍길동 님의 회원정보를 안전하게 보호하기 위해 비밀번호를 한번 더
+              확인해 주세요.
+            </p>
+          </p>
         </p>
         <p className="mypage-infochange-passwordinput">
           <input type="password" className="mypage-infochange-input" />
--- front/src/components/mypage/InfoChangeStep2.jsx
+++ front/src/components/mypage/InfoChangeStep2.jsx
@@ -121,107 +121,25 @@ export default function InfoChangeStep2({ nextstep }) {
         <tr>
           <td></td>
           <td>
-            <input type="text" className="editprofile-table-inputsubsub" />년
-            <input type="text" className="editprofile-table-inputsubsub" />월
-            <input type="text" className="editprofile-table-inputsubsub" />일
+            <input
+              type="text"
+              className="editprofile-table-inputsubsubsubsub"
+            />
+            년
+            <input
+              type="text"
+              className="editprofile-table-inputsubsubsubsub"
+            />
+            월
+            <input
+              type="text"
+              className="editprofile-table-inputsubsubsubsub"
+            />
+            일
           </td>
         </tr>
       </table>
-      {/* <p className="profileedit">
-        <p className="profileedit-id">
-          <p>아이디</p>
-          <input type="text" />
-        </p>
-        <p className="profileedit-pass">
-          <p>현재 비밀번호</p>
-          <input type="password" />
-        </p>
-        <p className="profileedit-newpass">
-          <p>새 비밀번호</p>
-          <input type="password" />
-        </p>
-        <p className="profileedit-newpasscheck">
-          <p>새 비밀번호 확인</p>
-          <input type="password" />
-        </p>
-        <p className="profileedit-name">
-          <p>이름</p>
-          <input type="text" />
-        </p>
-        <p className="profileedit-address">
-          <p>주소</p>
-          <p>
-            <div>
-              <input type="text" name="zipcode" placeholder="우편번호" />
-              <button type="button">주소검색</button>
-            </div>
-            <p>
-              <input type="text" />
-            </p>
-            <p>
-              <input type="text" />
-            </p>
-          </p>
-        </p>
-        <p className="profileedit-phone">
-          <p>휴대전화</p>
-          <select name="" id="">
-            <option value="">010</option>
-          </select>
-          <input type="text" />
-          <input type="text" />
-        </p>
-        <p className="profileedit-sms">
-          <p>SMS 수신여부</p>
-          <p>
-            <input type="radio" /> 수신함
-          </p>
-          <p>
-            <input type="radio" /> 수신안함
-          </p>
-        </p>
-        <p className="profileedit-email">
-          <p>이메일</p>
-          <input type="text" />
-          <p>@</p>
-          <select name="" id="">
-            <option value="">직접입력</option>
-            <option value="">네이버</option>
-            <option value="">구글</option>
-            <option value="">MS</option>
-          </select>
-        </p>
-        <p className="profileedit-emailmessage">
-          <p>이메일 수신여부</p>
-          <p>
-            <input type="radio" /> 수신함
-          </p>
-          <p>
-            <input type="radio" /> 수신안함
-          </p>
-        </p>
-        <p className="profileedit-gender">
-          <p>성별</p>
-          <p>
-            <input type="radio" /> 남자
-          </p>
-          <p>
-            <input type="radio" /> 여자
-          </p>
-        </p>
-        <p className="profileedit-birth">
-          <p>생년월일</p>
-          <p>
-            <input type="radio" /> 양력
-            <input type="radio" /> 음력
-            <p>
-              <input type="text" />
-              <input type="text" />
-              <input type="text" />
-            </p>
-          </p>
-        </p>
-      </p> */}
+
       <p className="profileedit-buttons">
         <button className="profileedit-button">Edit Account</button>
         <button className="profileedit-button">Delete Account</button>
