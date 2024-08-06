f89e4b688fa66f0a9bd5bda9713d8a0422f3b9a2
seo <test@test.com>
2024-08-06 15:48:31 +0900

    commit 0806 kim

--- front/src/components/SuccessAddtocartModal.jsx
+++ front/src/components/SuccessAddtocartModal.jsx
@@ -1,12 +1,18 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTimes } from '@fortawesome/free-solid-svg-icons';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-import Pagination from 'rc-pagination';
-import 'rc-pagination/assets/index.css';
+import React, { useEffect, useRef, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import Pagination from "rc-pagination";
+import "rc-pagination/assets/index.css";
+import "../css/cart.css";
 
-export default function SuccessAddtocartModal({ closeModal, prop, tp, choice }) {
+export default function SuccessAddtocartModal({
+  closeModal,
+  prop,
+  tp,
+  choice,
+}) {
   const modalBackground = useRef();
   const [cart, setCart] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
@@ -19,14 +25,14 @@ export default function SuccessAddtocartModal({ closeModal, prop, tp, choice }) 
         const response = await axios.post(url, { userId: choice.userId });
         setCart(response.data);
       } catch (error) {
-        console.error('Error fetching cart data:', error);
+        console.error("Error fetching cart data:", error);
       }
     };
     fetchData();
-  }, [choice.userId,cart]);
+  }, [choice.userId, cart]);
 
   console.log(cart);
-  
+
   const handleCloseModal = () => {
     closeModal();
   };
@@ -45,20 +51,30 @@ export default function SuccessAddtocartModal({ closeModal, prop, tp, choice }) 
 
   return (
     <>
-      <div className="modal-container" ref={modalBackground} onClick={(e) => {
-        if (e.target === modalBackground.current) {
-          handleCloseModal();
-        }
-      }}>
+      <div
+        className="modal-container"
+        ref={modalBackground}
+        onClick={(e) => {
+          if (e.target === modalBackground.current) {
+            handleCloseModal();
+          }
+        }}
+      >
         <div className="modal-content">
           <p className="Add-cart-title">Add to cart</p>
           <span className="cart-modal-close-btn" onClick={handleCloseModal}>
             <FontAwesomeIcon icon={faTimes} />
           </span>
-          <ul className="image-list"> {/* 가로로 2개씩 나열할 이미지들을 감싸는 ul 요소 */}
+          <ul className="image-list">
+            {" "}
+            {/* 가로로 2개씩 나열할 이미지들을 감싸는 ul 요소 */}
             {getCurrentItems().map((obj) => (
               <li key={obj.pid}>
-                <img className="cart-result-img" src={`http://127.0.0.1:8080/${obj.pimage}`} alt={obj.pname} />
+                <img
+                  className="cart-modal-result-img"
+                  src={`http://127.0.0.1:8080/${obj.pimage}`}
+                  alt={obj.pname}
+                />
                 <ul className="cart-modal-info">
                   <li>{obj.pname}</li>
                   <li>{obj.pdetail}</li>
@@ -66,11 +82,9 @@ export default function SuccessAddtocartModal({ closeModal, prop, tp, choice }) 
                   <li>{obj.pprice}</li>
                   <li>수량 : {obj.qty}</li>
                 </ul>
-                
               </li>
-             
             ))}
-            </ul>
+          </ul>
 
           <div className="pagination-container">
             <Pagination
@@ -83,7 +97,9 @@ export default function SuccessAddtocartModal({ closeModal, prop, tp, choice }) 
           </div>
 
           <div className="button-group">
-            <button className="modal-keep-shopping" onClick={handleCloseModal}>계속 쇼핑하기</button>
+            <button className="modal-keep-shopping" onClick={handleCloseModal}>
+              계속 쇼핑하기
+            </button>
             <Link to="/cart">
               <button className="modal-go-cart">장바구니로 가기</button>
             </Link>
